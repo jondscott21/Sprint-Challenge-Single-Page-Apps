16c1584ba697cc3d7da1a55047cf26a857bd681a
Jonathan Scott <jonathan.scott@gmail.com>
2019-07-26 08:56:27 -0700

    character cards displaying correctly

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,15 @@
 import React from 'react'
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard ({ character }) {
+  console.log(character)
+  return (
+    <div className="character-card">
+      <img src={character.image}></img>
+      <h2>{character.name}</h2>
+      <p>{character.species} {character.status}</p>
+      <p>Location: {character.location.name}</p>
+      <p>Origin: {character.origin.name}</p>
+    </div>
+    );
+  
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 export default function LocationsList() {
+    const [locations, setLocations] = useState([])
+    useEffect(() => {
+        // TODO: Add AJAX/API Request here - must run in `useEffect`
+        //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+        axios.get('https://rickandmortyapi.com/api/location/')
+        .then(res => {
+          console.log(res.data.results);
+          setLocations(res.data.results)
+        })
+      }, [])
+
+      return (
+        <div></div>
+      );
 
 }
